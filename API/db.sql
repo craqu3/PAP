1c634e4279d237a938f18adf941b597fdf9e17e4
unknown <a.rochagabri@gmail.com>
2026-03-01 23:39:07 +0000

    last update PAP

--- API/db.sql
+++ API/db.sql
@@ -19,19 +19,6 @@
 CREATE DATABASE IF NOT EXISTS `pap` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `pap`;
 
--- A despejar estrutura para tabela pap.app_ratings
-CREATE TABLE IF NOT EXISTS `app_ratings` (
-  `ID` int DEFAULT NULL,
-  `USER_ID` int DEFAULT NULL,
-  `firstName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `lastName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `stars` smallint DEFAULT NULL,
-  `feedback` smallint DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- A despejar dados para tabela pap.app_ratings: ~0 rows (aproximadamente)
-
 -- A despejar estrutura para tabela pap.orders
 CREATE TABLE IF NOT EXISTS `orders` (
   `ID` int DEFAULT NULL,
@@ -41,13 +28,6 @@ CREATE TABLE IF NOT EXISTS `orders` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- A despejar dados para tabela pap.orders: ~0 rows (aproximadamente)
-
--- A despejar estrutura para tabela pap.restaurant
-CREATE TABLE IF NOT EXISTS `restaurant` (
-  `ID` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- A despejar dados para tabela pap.restaurant: ~0 rows (aproximadamente)
 
 -- A despejar estrutura para tabela pap.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -70,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- A despejar dados para tabela pap.users: ~2 rows (aproximadamente)
 INSERT INTO `users` (`ID`, `firstName`, `lastName`, `email`, `password`, `recEmail`, `gender`, `role`, `created_at`, `update_at`, `token`, `tokenTime`) VALUES
 	(1, 'teste', 'test', 'teste', 'teste', 'tes', 'male', 'restaurant', '2026-02-06 15:39:41', '2026-02-06 15:39:43', NULL, NULL),
-	(2, 'Testeee', 'Teste', 'a.rochagabri@gmail.com', '$2b$12$pKN.3Wv9izyvkNuNWNU6Seo4JrTo9m.ZQOlD9i4SI8xjZtL2HoEum', NULL, NULL, 'client', '2026-02-06 16:41:11', '2026-02-22 16:36:13', NULL, NULL);
+	(2, 'Testeee', 'Teste', 'a.rochagabri@gmail.com', '$2b$12$eq4qAm3IXbJiXXp32B8TVu6tSDwn2UPx99d36NE0TBiq1gmptXn9i', NULL, NULL, 'client', '2026-02-06 16:41:11', '2026-02-26 21:51:01', NULL, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
